68dfb427828c5a0418dd23c827ae76a269e451e8
Evan Varozza <VarozzaEJ@gmail.com>
2024-03-26 01:11:42 -0600

    commit

--- balloonpop/main.js
+++ balloonpop/main.js
@@ -16,7 +16,7 @@ let currentPopCount = 0
 let gameLength = 5000
 let clockId = 0
 let timeRemaining = 0
-
+let currentPlayer = {}
 
 function startGame(){
 
@@ -72,7 +72,7 @@ function draw(){
 
   clickCountElem.innerText = clickCount.toString()
   popCountElem.innerText = currentPopCount.toString()
-  highPopCountElem.innerText = highestPopCount.toString()
+  highPopCountElem.innerText = currentPlayer.topScore.toString()
 }
 
 function stopGame(){
@@ -85,8 +85,9 @@ function stopGame(){
   height = 120
   width = 100
 
-  if(currentPopCount > highestPopCount){
-    highestPopCount = currentPopCount
+  if(currentPopCount > currentPlayer.topScore){
+    currentPlayer.topScore = currentPopCount
+    savePlayers()
   }
   currentPopCount = 0
 
@@ -96,12 +97,41 @@ function stopGame(){
 // #endregion
 
 let players = []
+loadPlayers()
 
 function setPlayer(event){
   event.preventDefault()
   let form = event.target
 
   let playerName = form.playerName.value
-  form.reset()
 
+  currentPlayer = players.find(player => player.name == playerName)
+
+  if(!currentPlayer) {
+    currentPlayer = {name: playerName, topScore: 0}
+    players.push(currentPlayer)
+    savePlayers()
+  }
+
+ 
+
+  form.reset()
+  document.getElementById("game")?.classList.remove("hidden")
+  form.classList.add("hidden")
+  draw()
+}
+
+function changePlayer(){
+  document.getElementById("player-form").classList.remove("hidden")
+  document.getElementById("game").classList.add("hidden")
+}
+
+function savePlayers(){
+  window.localStorage.setItem("players", JSON.stringify(players))
+}
+function loadPlayers(){
+  let playersData = JSON.parse(window.localStorage.getItem("players"))
+  if(playersData){
+    players = playersData
+  }
 }
